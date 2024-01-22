9c9b10122bb60d42dd534062d76da7cdc262df55
Ella Jameson <ellagjameson@gmail.com>
2024-01-21 19:54:43 -0700

    Update draw.py

--- src/tspart/draw.py
+++ src/tspart/draw.py
@@ -3,11 +3,7 @@ from PIL import Image, ImageDraw
 
 
 def map_route_points(points, route):
-    result = np.ndarray
-    for idx in route:
-        np.append(result, points[idx])
-
-    return result
+    return np.array([points[idx] for idx in route])
 
 
 def draw_points(points, size=None, background=(255, 255, 255), foreground=(0, 0, 0), radius=2):
@@ -29,5 +25,44 @@ def draw_points(points, size=None, background=(255, 255, 255), foreground=(0, 0,
             fill=foreground,
             outline=None
         )
+
+    return img
+
+
+def draw_route(points, route, size=None, background=(255, 255, 255), foreground=(0, 0, 0), line_width=2):
+    if size is None:
+        size = tuple(np.ceil(points.max(0)).astype(int) + 1)
+
+    img = Image.new(mode="RGB", size=size, color=background)
+    draw = ImageDraw.Draw(img)
+
+    route_points = map_route_points(points, route)
+
+    last_point = None
+    for point in route_points:
+        point = tuple(point.round().astype(int))
+
+        # Draw dot
+        '''
+        radius = line_width
+        draw.ellipse(
+            xy=(
+                (round(point[0]-radius), round(point[1]-radius)),
+                (round(point[0]+radius), round(point[1]+radius))
+            ),
+            fill=foreground,
+            outline=None
+        )
+        '''
+
+        # Draw line
+        if last_point is not None:
+            draw.line(
+                xy=(last_point, point),
+                fill=foreground,
+                width=line_width
+            )
+
+        last_point = point
 
     return img
