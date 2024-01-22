c1cf06d508183d6fe9f031557114b6c491f8df0b
Ella Jameson <ellagjameson@gmail.com>
2024-01-22 15:51:18 -0700

    Fixed CMYK channels being inverted

--- src/tspart/_image.py
+++ src/tspart/_image.py
@@ -1,25 +1,21 @@
 import numpy as np
-from PIL import Image
 
 
 def split_cmyk(rgb_array, threshhold=1):
     data = rgb_array.astype(float) / 255
-    minimum_black_value = threshhold / 255
+    threshold = threshhold / 255
 
     channel_max = data.max(2)
-    channel_max[channel_max < minimum_black_value] = minimum_black_value
+    channel_max[channel_max < threshold] = threshold
 
     k = 1 - channel_max
     c = (1 - data[:, :, 0] - k) / channel_max
     m = (1 - data[:, :, 1] - k) / channel_max
     y = (1 - data[:, :, 2] - k) / channel_max
 
-    result = [c, m, y, k]
+    result = 1 - np.array([c, m, y, k])
 
-    for idx, value in enumerate(result):
-        result[idx] = (result[idx] * 255).round().astype(int)
-
-    return tuple(result)
+    return tuple([(_ * 255).round().astype(int) for _ in result])
 
 
 def split_rgb(rgb_array):
