e3930d4807f401c9b086e62c92e88d74cbc2ac21
Ella Jameson <ellagjameson@gmail.com>
2024-01-21 19:54:47 -0700

    Update _tsp.py

--- src/tspart/_tsp.py
+++ src/tspart/_tsp.py
@@ -5,13 +5,8 @@ from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
 
 # See https://developers.google.com/optimization/routing/tsp
-def solve(points, closed=False):
+def solve(points, closed=False, log=True, verbose=False):
     distance_matrix = scipy.spatial.distance.cdist(points, points)
-
-    '''
-    if not closed:
-        distance_matrix[:, 0] = 0
-    '''
 
     if closed:
         manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, 0)
@@ -19,7 +14,12 @@ def solve(points, closed=False):
         end = random.choice([_ for _ in range(1, len(points))])
         manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, [0], [end])
 
-    routing = pywrapcp.RoutingModel(manager)
+    routing_parameters = pywrapcp.DefaultRoutingModelParameters()
+    if log and verbose:
+        routing_parameters.solver_parameters.trace_propagation = True
+        routing_parameters.solver_parameters.trace_search = True
+
+    routing = pywrapcp.RoutingModel(manager, routing_parameters)
 
     def distance_callback(from_idx, to_idx):
         from_node = manager.IndexToNode(from_idx)
@@ -34,14 +34,21 @@ def solve(points, closed=False):
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
     )
 
-    search_parameters.log_search = True
+    if log:
+        search_parameters.log_search = True
 
     solution = routing.SolveWithParameters(search_parameters)
 
-    index = routing.Start(0)
-    route = np.ndarray([manager.IndexToNode(index)])
-    while not routing.IsEnd(index):
-        index = solution.Value(routing.NextVar(index))
-        np.append(route, manager.IndexToNode(index))
+    routes = []
+    for route_nbr in range(routing.vehicles()):
+        index = routing.Start(route_nbr)
+        route = [manager.IndexToNode(index)]
+        while not routing.IsEnd(index):
+            index = solution.Value(routing.NextVar(index))
+            route.append(manager.IndexToNode(index))
+        routes.append(route)
 
-    return route
+    if len(routes) > 0:
+        return routes[0]
+    else:
+        return None
