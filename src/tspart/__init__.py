ae231c37d807d54604e251a3bfe8b541332468e2
Ella Jameson <ellagjameson@gmail.com>
2024-01-24 00:09:12 -0700

    voronoi restructure

--- src/tspart/__init__.py
+++ src/tspart/__init__.py
@@ -1,19 +1,15 @@
 """TSP Art Module for Python"""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
-from tspart._stippler import stipple, stipple_multi
-from tspart._draw import draw_points, draw_cmyk_points, draw_route, draw_cmyk_routes
-from tspart._image import split_rgb, split_cmyk
-from tspart._helpers import (
-    image_to_array, map_points_to_route, map_points_to_route_multi, ndarray_to_array_2d, array_to_ndarray_2d,
-    nearest_point_index, rgb_array_size, luminance
-)
-from tspart._transform import transform, transform_cmyk
-from tspart._files import (
-    save_json, load_json, save_routes, load_routes, load_image_as_array, save_tsplib, load_cyc_route
-)
 
 import tspart.voronoi
 import tspart.tsp
 import tspart.neos
+
+from tspart._draw import draw_points, draw_cmyk_points, draw_route, draw_cmyk_routes
+from tspart._image import split_rgb, split_cmyk
+from tspart._helpers import image_to_array, map_points_to_route, map_points_to_route_multi
+from tspart._files import (
+    save_json, load_json, save_routes, load_routes, load_image_as_array, save_tsplib, load_cyc_route
+)
--- src/tspart/voronoi.py
+++ src/tspart/voronoi.py
@@ -246,3 +246,79 @@ def centroids(points, density, density_P=None, density_Q=None):
 
         centroids.append(centroid)
     return regions, np.array(centroids)
+
+# ---- END OF ORIGINAL FILE ---- #
+import sys
+import scipy.ndimage
+
+from etatime import EtaBar
+
+
+def normalize(D):
+    Vmin, Vmax = D.min(), D.max()
+    if Vmax - Vmin > 1e-5:
+        D = (D - Vmin) / (Vmax - Vmin)
+    else:
+        D = np.zeros_like(D)
+    return D
+
+
+def initialization(n, D):
+    """
+    Return n points distributed over [xmin, xmax] x [ymin, ymax]
+    according to (normalized) density distribution.
+
+    with xmin, xmax = 0, density.shape[1]
+         ymin, ymax = 0, density.shape[0]
+
+    The algorithm here is a simple rejection sampling.
+    """
+
+    samples = []
+    while len(samples) < n:
+        # X = np.random.randint(0, D.shape[1], 10*n)
+        # Y = np.random.randint(0, D.shape[0], 10*n)
+        X = np.random.uniform(0, D.shape[1], 10 * n)
+        Y = np.random.uniform(0, D.shape[0], 10 * n)
+        P = np.random.uniform(0, 1, 10 * n)
+        index = 0
+        while index < len(X) and len(samples) < n:
+            x, y = X[index], Y[index]
+            x_, y_ = int(np.floor(x)), int(np.floor(y))
+            if P[index] < D[y_, x_]:
+                samples.append([x, y])
+            index += 1
+    return np.array(samples)
+
+
+def stipple_image(grayscale_array, points=5000, iterations=50):
+    # We want (approximately) 500 pixels per voronoi region
+    zoom = (points * 500) / (grayscale_array.shape[0] * grayscale_array.shape[1])
+    density = scipy.ndimage.zoom(grayscale_array, zoom, order=0)
+
+    density = 1.0 - normalize(density)
+    density_P = density.cumsum(axis=1)
+    density_Q = density_P.cumsum(axis=1)
+
+    # Initialization
+    points = initialization(points, density)
+
+    for i in EtaBar(range(iterations)):
+        regions, points = centroids(points, density, density_P, density_Q)
+
+    return points / zoom
+
+
+def stipple_image_multi(grayscale_arrays, points=5000, iterations=50):
+    result = []
+    for idx, grayscale_array in enumerate(grayscale_arrays):
+        print(f"Stippling image {idx + 1}/{len(grayscale_arrays)}", file=sys.stderr)
+        stippled = stipple_image(
+            grayscale_array=grayscale_array,
+            points=points,
+            iterations=iterations
+        )
+
+        result.append(stippled)
+
+    return result
